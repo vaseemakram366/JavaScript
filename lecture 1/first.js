bac3a201e1965c8032e258a75fbe5b70a0fcc3bf
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-08 15:27:17 +0530

    learned variables in javaScript

---
+++ lecture 1/first.js
@@ -0,0 +1,32 @@
+// name = "Vaseem Akram";
+// age = 24;
+// price = 1324;
+// x = null;
+// y = undefined;
+// isFollow = false;
+// isFollow = true;
+// name = 25
+// A = 10
+
+// _1 ="America"
+
+// fullname = "Vaseem Akram"
+// _fullName = "Naseem"
+// $fullname = "Sahib"
+// consolo = 12 + 34
+
+// var fullName = "Vaseem Akram"
+// var age = 19;
+// // var totalPrice = 9348;
+// let age = 32;
+// let age = 34;
+// let age = 36;
+
+
+// let age = 32;
+// age = 34;
+// age = 36;
+
+const age;
+
+console.log(age);
