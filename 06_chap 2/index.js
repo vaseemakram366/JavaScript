982196e3147f2fe46a3382a70eb1d65d08c72cfc
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-26 23:13:05 +0530

    Learned assignment and comparison operators

--- 06_chap 2/index.js
+++ 06_chap 2/index.js
@@ -1,28 +1,39 @@
-console.log("Operator in JS");
+// console.log("Operator in JS");
 
-// Arithmatic Operators
-let a = 45;
-let b = 4;
+// // Arithmatic Operators
+// let a = 45;
+// let b = 4;
 
-console.log("a+b = ", a+b);
-console.log("a-b = ", a-b);
-console.log("a/b = ", a/b);
-console.log("a ** b = ", a**b);
-console.log("a % b =", a%b);
+// console.log("a+b = ", a+b);
+// console.log("a-b = ", a-b);
+// console.log("a/b = ", a/b);
+// console.log("a ** b = ", a**b);
+// console.log("a % b =", a%b);
 
-console.log("++a  =", ++a);//46
-console.log("a ++  =", a++);//46
-console.log("--a =", --a);//46
-console.log("a =",a);//46
-console.log("a--", a--);//46
+// console.log("++a  =", ++a);//46
+// console.log("a ++  =", a++);//46
+// console.log("--a =", --a);//46
+// console.log("a =",a);//46
+// console.log("a--", a--);//46
 
 // Assignment Operators
-a = 8;
-console.log(a);
+// a += 8// a = a + 8;
+// console.log(a);
 
+// let assignment = 1;
+// assignment+=6;
+// console.log("a is now",a);
+// assignment+=6;
+// console.log("a is now",a);
+// assignment*=6;
+// console.log("a is now",a);
+// assignment/=6;
+// console.log("a is now",a);
 
-
-
-
-
-
+// Comparison Operators
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is", comp1 == comp2);
+console.log("comp1 == comp2 is", comp1 != comp2);
+console.log("comp1 == comp2 is", comp1 === comp2);
+console.log("comp1 == comp2 is", comp1 !== comp2);
