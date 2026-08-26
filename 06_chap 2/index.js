63c70a0767f61780c48709695ba49d3c9fc92b68
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-26 22:41:37 +0530

    Learned arithmatic operators

---
+++ 06_chap 2/index.js
@@ -0,0 +1,28 @@
+console.log("Operator in JS");
+
+// Arithmatic Operators
+let a = 45;
+let b = 4;
+
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a/b = ", a/b);
+console.log("a ** b = ", a**b);
+console.log("a % b =", a%b);
+
+console.log("++a  =", ++a);//46
+console.log("a ++  =", a++);//46
+console.log("--a =", --a);//46
+console.log("a =",a);//46
+console.log("a--", a--);//46
+
+// Assignment Operators
+a = 8;
+console.log(a);
+
+
+
+
+
+
+
