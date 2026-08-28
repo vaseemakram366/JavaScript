3443c88c8750e24df29b4720ca6e50505226aaaa
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-28 13:51:21 +0530

    Solved questions on prompt () and ternary operators

---
+++ 07_chap 2 PS/index.js
@@ -0,0 +1,58 @@
+// chap 2 ps
+// problem no. 1
+/*let age = prompt("What is your age?")
+if(age>10 && age<20){
+    console.log("Your age lies between 10 and 20");
+    
+}
+else{
+    console.log("your age doesnt liesm between 10 and 20");
+    
+}*/
+
+// prob 2
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question("What is your age? ", (age) => {
+//     console.log("Your age is:", age);
+//     rl.close();
+// // });
+// let age = prompt("what is your age?")
+// switch(age){
+//     case '12':
+//         console.log("Your age is 12");
+//         break
+//     case '13':
+//         console.log("Your age is 13");
+//         break
+//     case '14':
+//         console.log("Your age is 14");
+//         break
+//     case '15':
+//         console.log("Your age is 15");
+//         break
+//     default:
+//         console.log("Your age is not special");   
+// }
+
+// // problem no. 3
+// let num = prompt("What is your age?")
+// num = Number.parseInt(num)
+// if(num%2==0 && num%3 == 0) {
+//     console.log("Your number is divisible by 2 and 3");
+    
+// }
+// else {
+//     console.log("Your number is not divisible by 2 and 3");
+    
+// }
+
+// problem no. 4
+let age = 10
+let a = age>18?"you can drive":"you cannot drive"
+console.log(a);
