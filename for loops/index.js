391c4e35d712129b8cc957dbcc74e4689eb0353b
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-29 10:41:43 +0530

    Add for...in loop examples

--- for loops/index.js
+++ for loops/index.js
@@ -1,9 +1,31 @@
-let sum = 0
-let n = prompt("Enter the value of n")
-n = Number.parseInt(n)
-for (let i = 0; i<n; i++){
-    sum += (i+1)
+// for loop
+
+// let sum = 0
+// let n = prompt("Enter the value of n")
+// n = Number.parseInt(n)
+// for (let i = 0; i<n; i++){
+//     sum += (i+1)
     
+// }
+
+// console.log("Sum of first" + n + "natural numbers is : " + sum);
+
+// let n = 5
+// for (i = 0; n<5; i++){
+//     console.log(i);
+    
+// }
+
+// for in loop
+
+let obj = {
+    vaseem:90,
+    shubh:45,
+    shibika:34,
+    shiv:23
 }
 
-console.log("Sum of first" + n + "natural numbers is : " + sum);
+for (let a in obj){
+    console.log("Marks of" + a + " are " + obj[a]);
+    
+}
