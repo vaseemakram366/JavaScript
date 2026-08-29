1d9d5ff8fa242b608aed8b916869193e3ee5a21b
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-29 10:50:41 +0530

    Add for...of loop examples

--- for loops/index.js
+++ for loops/index.js
@@ -9,6 +9,7 @@
 // }
 
 // console.log("Sum of first" + n + "natural numbers is : " + sum);
+// console.log(i);
 
 // let n = 5
 // for (i = 0; n<5; i++){
@@ -18,14 +19,20 @@
 
 // for in loop
 
-let obj = {
-    vaseem:90,
-    shubh:45,
-    shibika:34,
-    shiv:23
-}
+// let obj = {
+//     vaseem:90,
+//     shubh:45,
+//     shibika:34,
+//     shiv:23
+// }
 
-for (let a in obj){
-    console.log("Marks of" + a + " are " + obj[a]);
+// for (let a in obj){
+//     console.log("Marks of" + a + " are " + obj[a]);
     
-}
+// }
+
+// // for of loop
+
+// for (let b of "vaseem"){
+//     console.log(b);
+// }
