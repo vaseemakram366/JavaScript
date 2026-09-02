8f8cd67459499ce315be0551a126dea51dacb183
Vaseem Akram <vaseemakrama366@gmail.com>
2026-09-02 22:35:39 +0530

    feat: add examples of JavaScript strings and template literals

---
+++ strings in js/index.js
@@ -0,0 +1,27 @@
+// string in js
+
+let name = "Harry";
+// console.log(name.length);
+console.log(name[0]);
+console.log(name[1]);
+let friend = 'Prakash'
+console.log(friend);
+// let myfriend = "Shubh';
+// console.log(myfriend);
+
+// Template literals
+
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+// Nikhil is a friend of pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence);
+
+// Escape sequence characters
+// let fruit = 'bana\'na'
+// console.log(fruit);
+let fruit = "bana\"na"
+console.log(fruit);
+
+
+
