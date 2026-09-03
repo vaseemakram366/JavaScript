0f41e62b52f3e13bac96465d28a3287cef34ceaa
Vaseem Akram <vaseemakrama366@gmail.com>
2026-09-03 19:24:52 +0530

    feat: add examples of JavaScript string methods and indexing

---
+++ string methods/index.js
@@ -0,0 +1,22 @@
+let name = "Harry"
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(2, 4));
+// console.log(name.slice(2));
+// console.log(name.replace("har", "per"));
+
+// let friend = "Aman"
+// console.log(name.concat(" is a friend of ", friend, "ok"));
+
+// let friend2 = "           arpit              "
+// console.log(friend2);
+// console.log(friend2.trim());
+
+let fr = "Shivika"
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
+
+// QuickQuiz: Use a for loop to print a string
