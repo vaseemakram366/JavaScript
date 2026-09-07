bf1aeca23aa2429b024b3986ecbdfb4a91b360fc
Vaseem Akram <vaseemakrama366@gmail.com>
2026-09-07 21:37:12 +0530

    feat: demonstrate JavaScript array iteration methods

---
+++ Using loops with arrays/index.js
@@ -0,0 +1,28 @@
+let num = [3, 54, 1, 2, 4]
+// for(let i = 0; i<num.length;i++){
+//     console.log(num[i]);
+// }
+
+// forEach loop
+// num.forEach((element)=>{
+//     console.log(element*element);
+    
+// })
+
+// Array.from
+
+// let name = "Vaseem"
+// let arr = Array.from(name)
+// console.log(arr);
+
+// for...of
+for (let i of num){
+    console.log(i);
+    
+}
+
+// for...in
+for (let i of num){
+    console.log(i);
+    
+}
