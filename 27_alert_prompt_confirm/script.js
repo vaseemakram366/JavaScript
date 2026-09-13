54f7da050ea0344d077d3ee672bed165354fa812
Vaseem Akram <vaseemakrama366@gmail.com>
2026-09-13 19:01:55 +0530

    feat: demonstrate JavaScript prompt, type conversion, confirmation, and document output

---
+++ 27_alert_prompt_confirm/script.js
@@ -0,0 +1,12 @@
+alert("Enter the value of a!")
+let a = prompt("Enter a here:", "578")
+a = Number.parseInt(a)
+alert("You entered a of tye " + (typeof a))
+let write = confirm("Do you want to write it to the page")
+if(write){
+    document.write(a)
+}
+else{
+    document.write("Please allow me to write")
+}
+document.write(a)
