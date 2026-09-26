563ab9719938f0670033a8ce6322a57c37ce1207
Vaseem Akram <vaseemakrama366@gmail.com>
2026-09-26 22:58:11 +0530

    feat: practice DOM innerHTML, outerHTML, textContent, and node properties

---
+++ 41_innerHTML_outerHTML/script.js
@@ -0,0 +1,30 @@
+// let x = document.getElementsByTagName('span')[0]
+// console.log(x);
+// let y = document.getElementsByTagName('span')[0]
+// console.dir(y);
+
+
+
+
+// console.log(document.body.firstChild.nodeName)
+// console.log(document.body.firstElementChild.nodeName)
+
+
+// first.outerHTML
+// '<span id="first"><b>hi</b>Hey i am span</span>'
+// first.outerHTML = "<div> Hey i am div</div>
+// VM1184:1 Uncaught SyntaxError: Invalid or unexpected token (at VM1184:1:19)
+// first.outerHTML = "<div> Hey i am div</div>"
+// '<div> Hey i am div</div>
+
+// document.body.firstChild
+// " Hello World "
+// document.body.firstChild.data
+// '\n    Hello World\n    '
+// document.body.firstChild.nodevalue
+// undefined
+// document.body.firstChild.nodeValue
+// '\n    Hello World\n    '
+
+// console.log(document.body.textContent);
+// first.hidden = false
